265e31b26d5fe5354201af3c7fb4624d97ebed9e
DashikiDon <bkibagendi@gmail.com>
2017-09-27 23:46:37 -0500

    finale

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import logo from "../logo.svg"
+class NavBar extends Component {
+  render () {
+    return (
+      <div className="mainHeader">
+        <div className="App-header">
+          <img src="http://cdn1.theodysseyonline.com/files/2015/12/19/6358614596527738711752945771_music.jpg" className="App-logo" alt="logo" />
+            <h1>PLAYLIST</h1>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default NavBar
